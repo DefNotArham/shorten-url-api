59f5773ae21c3003f89549f4a140248db647e0b5
DefNotArham <arhamkabir231@gmail.com>
2026-06-27 14:12:24 -0400

    added routes to server

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,17 @@ import express from "express";
 import dotenv from "dotenv";
 
 import connectDb from "./db/connectDb.js";
+import urlRoutes from "./routes/url.route.js";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
-connectDb();
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.use(express.json());
+app.use(urlRoutes);
+
+app.listen(port, async () => {
+  console.log(`Server is running on port ${port}`);
+  await connectDb();
+});
