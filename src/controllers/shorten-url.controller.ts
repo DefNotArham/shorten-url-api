c4ad26ab8974bcf2842f59e2c47f0a2c7e73f83e
DefNotArham <arhamkabir231@gmail.com>
2026-06-28 11:16:24 -0400

    finsihed shroten url ctronller

--- src/controllers/shorten-url.controller.ts
+++ src/controllers/shorten-url.controller.ts
@@ -3,6 +3,8 @@ import type { Request, Response } from "express";
 import ShortenUrl from "../model/url.model.js";
 import isValidUrl from "../utils/isValidUrl.js";
 
+import { nanoid } from "nanoid";
+
 const shortenUrlController = async (req: Request, res: Response) => {
   const { url } = req.body;
   try {
@@ -16,11 +18,21 @@ const shortenUrlController = async (req: Request, res: Response) => {
         .status(400)
         .json({ success: false, message: "Please enter a valid url" });
 
+    let shortCode: string = nanoid(6);
+
+    while (await ShortenUrl.findOne({ shortCode })) {
+      shortCode = nanoid(6);
+    }
+
     const shortenUrl = await ShortenUrl.create({
       originalUrl: url,
+      shortCode,
     });
+
+    res.status(200).json({ success: true, shortenUrl });
   } catch (error) {
     console.log(error);
+    res.status(500).json({ success: false, message: "Server error" });
   }
 };
 
