10918d100378bdde242d77936bebc4bfc11f167a
DefNotArham <arhamkabir231@gmail.com>
2026-06-28 11:11:01 -0400

    installed nanoid

--- src/controllers/shorten-url.controller.ts
+++ src/controllers/shorten-url.controller.ts
@@ -1,13 +1,24 @@
 import type { Request, Response } from "express";
+
 import ShortenUrl from "../model/url.model.js";
+import isValidUrl from "../utils/isValidUrl.js";
 
 const shortenUrlController = async (req: Request, res: Response) => {
   const { url } = req.body;
   try {
-    if (!url)
+    if (!url || typeof url !== "string")
       return res
         .status(400)
         .json({ success: false, message: "Please enter an url" });
+
+    if (!isValidUrl(url))
+      return res
+        .status(400)
+        .json({ success: false, message: "Please enter a valid url" });
+
+    const shortenUrl = await ShortenUrl.create({
+      originalUrl: url,
+    });
   } catch (error) {
     console.log(error);
   }
